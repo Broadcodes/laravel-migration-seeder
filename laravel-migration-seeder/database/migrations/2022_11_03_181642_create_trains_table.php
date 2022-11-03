e6a14e8d0caa72d998f5746471b15d4284947792
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-03 19:31:15 +0100

    Creazione di una tabella nel DB

---
+++ laravel-migration-seeder/database/migrations/2022_11_03_181642_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 30)->nullable(true);
+            $table->string('departureStation', 40)->nullable(false);
+            $table->string('arrivalStation', 40)->nullable(false);
+            $table->time('departureTime')->nullable(false);
+            $table->time('arrivalTime')->nullable(false);
+            $table->tinyInteger('trainCode')->nullable(true)->unsigned();
+            $table->tinyInteger('numberOfCarriages')->nullable(true)->unsigned();
+            $table->tinyInteger('inTime')->nullable(true);
+            $table->tinyInteger('deleted')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
